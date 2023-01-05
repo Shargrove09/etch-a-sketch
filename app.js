83f67bba6524fdfb94269406437080bb9034bf5e
Shargrove09 <scottie@hopestreamlmft.com>
2023-01-05 14:44:19 -0500

    Add hold down mouse btn to draw functionality Add clear btn Change variable names of container-grid

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const container = document.getElementById("container"); 
+const grid = document.getElementById("grid");
+const reloadrBtn = document.getElementById("clear-btn");
 
+reloadrBtn.onclick = () => reloadGrid();
 
-//Makes a grid of specficied size
-function makeGrid(rows, cols) { 
-  container.style.setProperty('--grid-rows', rows); 
-  container.style.setProperty('--grid-cols', cols); 
-  for (i = 0; i < (rows * cols); i++) { 
-    let cell = document.createElement("div"); 
-    container.appendChild(cell).className = "grid-item"; 
-  }
-
-  console.log("Ran")
+function reloadGrid() {
+  clearGrid();
+  makeGrid(16, 16);
 }
 
-makeGrid(16, 16)
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+//Makes a grid of specficied size
+function makeGrid(rows, cols) {
+  grid.style.setProperty("--grid-rows", rows);
+  grid.style.setProperty("--grid-cols", cols);
+  for (i = 0; i < rows * cols; i++) {
+    let cell = document.createElement("div");
+    cell.addEventListener("mousedown", changeColor);
+    cell.addEventListener("mouseover", changeColor);
+    grid.appendChild(cell).className = "grid-item";
+  }
+
+  console.log("Ran");
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+function changeColor(e) {
+  if (e.type == "mouseover" && !mouseDown) return;
+
+  e.target.style.backgroundColor = "black";
+}
+
+reloadGrid();
